e3f4b415ee9524807ba5aaca9e91ba7b773b918e
Loïc Denis <loic.alber.denis@gmail.com>
2020-02-07 11:09:37 +0100

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {MuseumService} from './services/openmuseum.service'
-
 import { SingleMuseumComponent } from './single-museum/single-museum.component';
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SingleMuseumComponent
+    SingleMuseumComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule
