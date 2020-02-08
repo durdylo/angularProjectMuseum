a489375ba68755dfcfb9aa97e395afb432ef15b4
Loïc Denis <loic.alber.denis@gmail.com>
2020-02-08 10:39:30 +0100

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes,RouterModule } from '@angular/router';
+import {AddMuseumComponent} from './add-museum/add-museum.component';
 import { AppComponent } from './app.component';
 import {MuseumService} from './services/openmuseum.service'
 import { SingleMuseumComponent } from './single-museum/single-museum.component';
 import { IndexComponent } from './index/index.component';
 
+
+const appRoutes:Routes = [
+  { path: 'add', component: AddMuseumComponent },
+  { path: 'singleMuseum', component: SingleMuseumComponent },
+  { path: '', component: IndexComponent }
+]
 @NgModule({
   declarations: [
     AppComponent,
     SingleMuseumComponent,
-    IndexComponent
+    IndexComponent,
+    AddMuseumComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [MuseumService],
   bootstrap: [AppComponent]
